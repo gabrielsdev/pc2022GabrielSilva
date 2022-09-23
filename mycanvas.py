cd0d28a3ee092d3194e5ba9dc36486f1fa002137
gabrielsdev <89107888+gabrielsdev@users.noreply.github.com>
2022-09-23 01:28:54 -0300

    bug fix

--- mycanvas.py
+++ mycanvas.py
@@ -34,10 +34,10 @@ class MyCanvas(QtOpenGL.QGLWidget):
     def resizeGL(self, _width, _height):
         self.m_w = _width
         self.m_h = _height
-        if(self.m_model==None)or(self.m_model.isEmpty()): 
+        if(self.m_hmodel==None)or(self.m_hmodel.isEmpty()): 
             self.scaleWorldWindow(1.0)
         else:
-            self.m_L,self.m_R,self.m_B,self.m_T = self.m_model.getBoundBox()
+            self.m_L,self.m_R,self.m_B,self.m_T = self.m_hmodel.getBoundBox()
             self.scaleWorldWindow(1.1)
         glViewport(0, 0, self.m_w, self.m_h)
         glMatrixMode(GL_PROJECTION)
@@ -49,18 +49,10 @@ class MyCanvas(QtOpenGL.QGLWidget):
 
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT)
-        if (self.m_model==None)or(self.m_model.isEmpty()):
-            return
         glCallList(self.list)
         glDeleteLists(self.list, 1)
         self.list = glGenLists(1)
         glNewList(self.list, GL_COMPILE)
-        '''verts = self.m_model.getVerts()
-        glColor3f(0.0, 1.0, 0.0) # green
-        glBegin(GL_TRIANGLES)
-        for vtx in verts:
-            glVertex2f(vtx.getX(), vtx.getY())
-        glEnd()'''
         pt0_U = self.convertPtCoordsToUniverse(self.m_pt0)
         pt1_U = self.convertPtCoordsToUniverse(self.m_pt1)
         glColor3f(1.0, 0.0, 0.0)
@@ -68,6 +60,7 @@ class MyCanvas(QtOpenGL.QGLWidget):
         glVertex2f(pt0_U.x(), pt0_U.y())
         glVertex2f(pt1_U.x(), pt1_U.y())
         glEnd()
+        glEndList()
 
 
         if not(self.m_hmodel.isEmpty()):
@@ -93,31 +86,14 @@ class MyCanvas(QtOpenGL.QGLWidget):
                 glVertex2f(ptc[1].getX(), ptc[1].getY())
                 glEnd()
 
-
-        '''if not((self.m_model == None) and (self.m_model.isEmpty())):
-            verts = self.m_model.getVerts()
-            glColor3f(0.0, 1.0, 0.0) # green
-            glBegin(GL_TRIANGLES)
-            for vtx in verts:
-                glVertex2f(vtx.getX(), vtx.getY())
-            glEnd()
-            curves = self.m_model.getCurves()
-            glColor3f(0.0, 0.0, 1.0) # blue
-            glBegin(GL_LINES)
-            for curv in curves:
-                glVertex2f(curv.getP1().getX(), curv.getP1().getY())
-                glVertex2f(curv.getP2().getX(), curv.getP2().getY())
-            glEnd()'''
-        glEndList()
-
     def setModel(self,_model):
         self.m_model = _model
 
     def fitWorldToViewport(self):
         print("Redimensionar")
-        if self.m_model == None:
+        if self.m_hmodel == None:
             return
-        self.m_L,self.m_R,self.m_B,self.m_T=self.m_model.getBoundBox()
+        self.m_L,self.m_R,self.m_B,self.m_T=self.m_hmodel.getBoundBox()
         self.scaleWorldWindow(1.10)
         self.update()
 
@@ -181,13 +157,7 @@ class MyCanvas(QtOpenGL.QGLWidget):
     def mouseReleaseEvent(self, event):
         pt0_U = self.convertPtCoordsToUniverse(self.m_pt0)
         pt1_U = self.convertPtCoordsToUniverse(self.m_pt1)
-        self.m_model.setCurve(pt0_U.x(),pt0_U.y(),pt1_U.x(),pt1_U.y())
-        #self.m_model.setCurve(self.m_pt0.x(),...,...,...)
         self.m_buttonPressed = False
-        self.m_pt0.setX(0)
-        self.m_pt0.setY(0)
-        self.m_pt1.setX(0)
-        self.m_pt1.setY(0)
         p0 = Point(pt0_U.x(),pt0_U.y())
         p1 = Point(pt1_U.x(),pt1_U.y())
         segment = Line(p0,p1)
